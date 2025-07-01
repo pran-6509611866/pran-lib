426f338ae276979430a793e0a2d6b27e09493e90
Pran Tinsulanonda <pran.tin@dome.tu.ac.th>
2025-07-01 16:49:45 +0700

    First Commit For Testing Package Library.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Say Hello Function
+
+A list of not so helpful funcions. This is just a test library.
---
+++ src/function.ts
@@ -0,0 +1,19 @@
+import { sayHelloProps } from "./type";
+
+export function sayHello({
+    firstName, 
+    lastName, 
+    age 
+} : sayHelloProps) {
+    console.log('Hello!');
+    console.log(`Your first name is ${firstName}`);
+    
+    if (lastName) {
+        console.log(`Your last name is ${lastName}`);
+
+    }
+
+    if (age) {
+        console.log(`Your age is ${age}`);
+    }
+}
